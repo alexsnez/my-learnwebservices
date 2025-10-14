17e2714a4eb90e713b101aa9180866c4908c520c
Aleksandr <alexnezdemina@gmail.com>
2025-10-14 21:46:20 +0300

    Create LearnWsApplication.java

---
+++ src/main/java/com/proanalitika/learnws/LearnWsApplication.java
@@ -0,0 +1,11 @@
+package com.proanalitika.learnws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearnWsApplication.class, args);
+    }
+}
